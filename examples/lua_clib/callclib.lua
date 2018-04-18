822cb7e63c37e02f17914fd3eaf1dc77c3f51b08
qy <qiyue@firevale.com>
2018-04-18 18:39:23 +0800

    add

--- examples/lua_clib/callclib.lua
+++ examples/lua_clib/callclib.lua
@@ -1,3 +1,4 @@
 local mylib = require("luaclib")
+
 a,s= mylib.average(1,2,3,4)
 print("average is " .. a.. " and sum is "..s);
--- examples/lua_clib/luaclib.c
+++ examples/lua_clib/luaclib.c
@@ -5,6 +5,7 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
+
 int average(lua_State *L)
 {
   int n = lua_gettop(L);
@@ -18,14 +19,31 @@ int average(lua_State *L)
   return 2;
 }
 
+//可以将c中定义的函数以数组的形式进行分组，这样require到lua中，可以以不同的名称调用
 const luaL_Reg mylib[] =
 {
   {"average", average},
   {"NULL", NULL}
 };
 
+static const luaL_Reg s_lib_preload[] = {
+  // { "socket.core", luaopen_socket_core },
+  // { "pb",    luaopen_pb }, // any 3rd lualibs added here
+  { NULL, NULL }
+};
+
+//lua 中require(#{filename})的时候，会调用名为luaopen_#{filename}的函数，所以该函数名很重要
 int luaopen_luaclib(lua_State *L)
 {
+  luaopen_socket_core(L);
   luaL_register(L, "mylib", mylib);
+
+  const luaL_Reg *lib;
+  lua_getglobal(L,"package");
+  lua_getfield(L,-1,"preload");
+  for (lib = s_lib_preload; lib->func; lib++) {
+    lua_pushcfunction(L, lib->func);
+    lua_setfield(L, -2, lib->name);
+  }
   return 1;
 }
